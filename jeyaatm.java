6d93b63836e8f65d4de9bef84ee95b6db35810b4
jeya0510 <60683131+jeya0510@users.noreply.github.com>
2026-02-24 12:33:01 +0530

    Updated jeyaatm.java

--- jeyaatm.java
+++ jeyaatm.java
@@ -1,47 +1,52 @@
 import java.util.*;
-class AtmTrans
-{
-	public static void main(String args[])
-	{
-		Scanner sc = new Scanner(System.in);	
-		AtmTrans a1 = new AtmTrans();	
-		char card='Y';
-		while(card=='Y')
-        	{
-		System.out.println("ATM..");
-		
-		int n=sc.next();
-		switch(n)
-		{
-			case 1:
-					a1.atm();		
-				
-			default:
-				System.out.Println("Please check and try again..");
-				break;
-			}
-			System.out.Println("Do you want to try again..?");
-				card='N';
-			}
-	}
-	public void atm()
-	{
-		int balance=10000, withdraw;
-		System.out.print("Enter money to be withdrawn:");
-                withdraw = sc.next();
-                if(balance >= withdraw)
-                {
-                    balance = balance - withdraw;
-                    System.out.println("Please collect your money..");
-                }
-		else
-                {
-                    System.out.println("Insufficient Balance..");
-                }
-                System.out.println("");
-		
-}
-}
 
+public class Main {
 
-            	
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String args[]) {
+
+        char card = 'Y';
+
+        while (card == 'Y' || card == 'y') {
+
+            System.out.println("===== ATM =====");
+            System.out.println("1. Withdraw");
+            System.out.println("Enter your choice:");
+
+            int n = sc.nextInt();
+
+            switch (n) {
+                case 1:
+                    atm();
+                    break;
+
+                default:
+                    System.out.println("Please check and try again..");
+                    break;
+            }
+
+            System.out.println("Do you want to try again? (Y/N)");
+            card = sc.next().charAt(0);
+        }
+
+        System.out.println("Thank you for using ATM!");
+    }
+
+    public static void atm() {
+
+        int balance = 10000;
+        int withdraw;
+
+        System.out.print("Enter money to be withdrawn: ");
+        withdraw = sc.nextInt();
+
+        if (balance >= withdraw) {
+            balance = balance - withdraw;
+            System.out.println("Please collect your money..");
+            System.out.println("Remaining balance: " + balance);
+        } else {
+            System.out.println("Insufficient Balance..");
+        }
+    }
+}
